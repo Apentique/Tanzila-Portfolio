459583034b1ea3db50bfc158b0126d0c201a6337
Apentique <88792675+Apentique@users.noreply.github.com>
2026-03-06 23:11:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Portfolio Loaded");
+window.addEventListener("scroll", function(){
+
+let nav = document.querySelector("nav");
+
+if(window.scrollY > 50){
+nav.style.background = "#ffffff";
+nav.style.boxShadow = "0 5px 15px rgba(0,0,0,0.1)";
+}else{
+nav.style.background = "transparent";
+nav.style.boxShadow = "none";
+}
+
+});
